c43670ba50084ce0381f3d9c497422ffdd001d23
João Santos <joaopns05@gmail.com>
2021-03-31 20:19:31 +0100

    explore concurrency and wait groups

--- routine-gen/main.go
+++ routine-gen/main.go
@@ -2,25 +2,59 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 
 func main() {
-  c := make(chan string)
 
-  for i := 0; i < 10; i++ {
-    go func(i int, c chan string){
-      goroutineName := "goroutine: " + strconv.Itoa(i)
-      c <- goroutineName
-    }(i, c)
+  type task struct {
+    id int
+    name string
   }
 
-  for i := 0; i < 10; i++ {
-    fmt.Println(<-c)
+  // Create task queue.
+  tasks := make(chan task)
+  workers := 2
+
+  // Create tasks.
+  go func() {
+    for i := 0; i < 10; i++ {
+      fmt.Println("iteration:", i)
+      tasks <- task{i, fmt.Sprintf("task %d", i)}
+    }
+    close(tasks)
+  }()
+
+  // Create a sync group with workers and schedule results close when all tasks are finished
+  var wg sync.WaitGroup
+  wg.Add(workers)
+  results := make(chan string)
+  go func() {
+    wg.Wait()
+    fmt.Println("tasks finished")
+    close(results)
+  }()
+
+  // Consume tasks in using workers
+  for i := 0; i < workers; i++ {
+    go func() {
+      defer wg.Done()
+
+      for t := range tasks {
+        seconds := rand.Intn(10)
+        fmt.Println("seconds", seconds)
+        time.Sleep(time.Second * time.Duration(seconds))
+        results <- t.name
+      }
+    }()
   }
 
-  fmt.Println("done")
-
+  for r := range results {
+    fmt.Println("received:", r)
+  }
+  fmt.Println("consumed all results")
 
 }
